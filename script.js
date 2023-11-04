991e56bbea76bfd4a4a8bb4f331f1d82982e04c2
sruti panda <srutirani666@gmail.com>
2023-11-04 20:06:09 +0530

    easy Quest

---
+++ script.js
@@ -0,0 +1,12 @@
+// JavaScript code for handling navigation links
+// document.addEventListener("DOMContentLoaded", function() {
+//     const navigationLinks = document.querySelectorAll("nav a");
+
+//     navigationLinks.forEach(link => {
+//         link.addEventListener("click", function(event) {
+//             event.preventDefault();
+//             const linkText = event.target.textContent;
+//             alert(`You clicked on '${linkText}'`);
+//         });
+//     });
+// });
